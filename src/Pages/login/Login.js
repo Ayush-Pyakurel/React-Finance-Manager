58c0dd6de08816a7be92cd7fc189fb2909a1ed35
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-11-24 10:46:46 +0545

    Login template with styles created

--- src/Pages/login/Login.js
+++ src/Pages/login/Login.js
@@ -1,8 +1,39 @@
 //style import
 import styles from './Login.module.css';
 
+//react hook import
+import { useState } from 'react';
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLoginFormSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
+  return (
+    <form className={styles['login-form']} onSubmit={handleLoginFormSubmit}>
+      <label>
+        <span>Email:</span>
+        <input
+          type='text'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
+  );
 };
 
 export default Login;
