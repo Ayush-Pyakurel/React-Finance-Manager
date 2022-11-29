8d0214b7ad9278892dc2b39d542c297e6466f237
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-11-29 14:46:03 +0545

    signUp using firebase authentication

--- src/Pages/signup/Signup.js
+++ src/Pages/signup/Signup.js
@@ -1,26 +1,35 @@
 //style import
-import styles from './Signup.module.css';
+import styles from "./Signup.module.css";
+
+//custome hook import
+import { useSignup } from "../../hooks/useSignup.js";
 
 //import react hooks
-import { useState } from 'react';
+import { useState } from "react";
 
 const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const { signUp, loading, error } = useSignup();
 
   //signup form submit
   const handleSignupFormSubmit = (e) => {
     e.preventDefault();
+    signUp(email, password, username);
+    setEmail(() => "");
+    setPassword(() => "");
+    setUsername(() => "");
   };
 
   return (
-    <form className={styles['signup-form']} onSubmit={handleSignupFormSubmit}>
+    <form className={styles["signup-form"]} onSubmit={handleSignupFormSubmit}>
       <h2>Sign Up</h2>
       <label>
         <span>Email: </span>
         <input
-          type='email'
+          type="email"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
           required
@@ -29,7 +38,7 @@ const Signup = () => {
       <label>
         <span>Password: </span>
         <input
-          type='password'
+          type="password"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
           required
@@ -38,13 +47,19 @@ const Signup = () => {
       <label>
         <span>Username: </span>
         <input
-          type='text'
+          type="text"
           onChange={(e) => setUsername(e.target.value)}
           value={username}
           required
         />
       </label>
-      <button className='btn'>Sign Up</button>
+      {!loading && <button className="btn">Sign Up</button>}
+      {loading && (
+        <button className="btn" disabled>
+          Loading.....
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 };
