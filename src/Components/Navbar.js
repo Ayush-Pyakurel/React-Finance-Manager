1f18a89ac9bb6c6344fcdce53a3d5197bf63e32f
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-11-30 17:44:36 +0545

    show login, signup, logout and display name as per login state in NavBar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,29 +3,41 @@ import styles from "./Navbar.module.css";
 
 //import custome hook
 import { useSignout } from "../hooks/uesLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 //import from react-router-dom
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { signout } = useSignout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>Finance Manager</li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
 
-        <li>
-          <button className="btn" onClick={signout}>
-            Logout
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hi {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={signout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
