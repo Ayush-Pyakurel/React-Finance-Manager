15a7a9edee838aabb14aa51dd4b982c77aeaeb84
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2022-12-04 10:23:49 +0545

    Implement route guard

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 //import './App.css';
 
 //import react router
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
 //import custome hooks
-import { useAuthContext } from "./hooks/useAuthContext";
+import { useAuthContext } from './hooks/useAuthContext';
 
 //import pages
-import Home from "./Pages/Home/Home";
-import Login from "./Pages/login/Login";
-import Signup from "./Pages/signup/Signup";
+import Home from './Pages/Home/Home';
+import Login from './Pages/login/Login';
+import Signup from './Pages/signup/Signup';
 
 //import component
-import Navbar from "./Components/Navbar";
+import Navbar from './Components/Navbar';
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
   return (
-    <div className="App">
+    <div className='App'>
       {/* we are checking with this condition to fix the flickering of the content */}
       {authIsReady && (
         <Router>
           <Navbar />
           <Switch>
-            <Route path="/" exact>
-              <Home />
+            {/* Adding route guard to protect the routes */}
+            <Route path='/' exact>
+              {!user && <Redirect to='/login' />}
+              {user && <Home />}
             </Route>
-            <Route path="/signup">
-              <Signup />
+            <Route path='/signup'>
+              {!user && <Signup />}
+              {user && <Redirect to='/' />}
             </Route>
-            <Route path="/login">
-              <Login />
+            <Route path='/login'>
+              {!user && <Login />}
+              {user && <Redirect to='/' />}
             </Route>
           </Switch>
         </Router>
